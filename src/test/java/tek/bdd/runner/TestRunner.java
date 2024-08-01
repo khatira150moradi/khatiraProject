e93c7a814e82fc8864276136346c7bee8563b269
Zikriya Qasimi <khatiramoradi150@gmil.com>
2024-08-01 19:46:51 -0400

    updated1

--- src/test/java/tek/bdd/runner/TestRunner.java
+++ src/test/java/tek/bdd/runner/TestRunner.java
@@ -1,4 +1,17 @@
 package tek.bdd.runner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 public class TestRunner {
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            features = "classpath:features/SecurityTest.feature", // Directory for Feature file
+            glue = "tek.bdd.steps", //Package for all the steps
+            dryRun = true //set to true to scan feature for unimplemented steps
+    )
+    public class TestRunner {
+    }
 }
